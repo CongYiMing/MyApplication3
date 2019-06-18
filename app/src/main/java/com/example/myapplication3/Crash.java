0f7e3b42c01ae5ee33d69b6aca2e6a2161bdbf7b
congyiming <congyiming@zhangyue.com>
2019-06-18 08:30:45 +0800

    v1.2改动

--- app/src/main/java/com/example/myapplication3/Crash.java
+++ app/src/main/java/com/example/myapplication3/Crash.java
@@ -6,6 +6,8 @@ public class Crash {
     public static String TAG = "lmy";
 
     public static void crash() {
+        Log.i(TAG, "1.2版本改动");
+        Log.i(TAG, "改动之后代码行数 就对不上了，但是方法和类还是能找到的  在方法和类中推测崩溃原因");
         int i = 1 / 0;
         Log.i(TAG, "i:" + i);
     }
